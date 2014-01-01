b3f84fcd38acb93ae67db444072b76369614086e
kobuz <kobbuz@gmail.com>
2014-01-01 10:08:01 +0100

    initial layout and home page

--- goclub/urls.py
+++ goclub/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', TemplateView.as_view(template_name='home.html'), name='home'),
 )
