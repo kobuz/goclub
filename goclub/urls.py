8c7f81e0df03fba50dc10db4d47f562d40b2f31c
kobuz <kobbuz@gmail.com>
2014-01-01 11:05:33 +0100

    about and members pages

--- goclub/urls.py
+++ goclub/urls.py
@@ -3,8 +3,12 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+from members.views import MemberList
+
 urlpatterns = patterns('',
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^klub$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^czlonkowie$', MemberList.as_view(), name='members'),
 )
--- members/views.py
+++ members/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+# coding=utf-8
+from django.views.generic import ListView
+
+from .models import Member
+
+
+class MemberList(ListView):
+    model = Member
+    template_name = 'members.html'
+    context_object_name = 'members'
