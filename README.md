6c790f132075dfa44c782affc037feace1c82224
Marcin Sokół <kobbuz@gmail.com>
2016-06-23 13:53:14 +0200

    update readme and urls

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 [![Build Status](https://travis-ci.org/kobuz/goclub.svg?branch=master)](https://travis-ci.org/kobuz/goclub)
 
 # goclub
+
+## Setup
+
+Install docker and docker-compose (version >=1.6.0) first.
+Run it with:
+
+```bash
+$> docker-compose up
+```
+
+It might be good idea to run migrations as well:
+
+```bash
+$> docker-compose run --rm web ./manage.py migrate
+```
--- goclub/goclub/urls.py
+++ goclub/goclub/urls.py
@@ -25,13 +25,13 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^klub$', TemplateView.as_view(template_name='about.html'),
+    url(r'^klub/$', TemplateView.as_view(template_name='about.html'),
         name='about'),
-    url(r'^spotkania$', TemplateView.as_view(template_name='meetings.html'),
+    url(r'^spotkania/$', TemplateView.as_view(template_name='meetings.html'),
         name='meetings'),
-    url(r'^spotkania/(?P<slug>.*)$', MeetingDetail.as_view(),
+    url(r'^spotkania/(?P<slug>.*)/$', MeetingDetail.as_view(),
         name='meeting_detail'),
-    url(r'^czlonkowie$', MemberList.as_view(), name='members'),
-    url(r'^kontakt$', TemplateView.as_view(template_name='contact.html'),
+    url(r'^czlonkowie/$', MemberList.as_view(), name='members'),
+    url(r'^kontakt/$', TemplateView.as_view(template_name='contact.html'),
         name='contact'),
 ]
